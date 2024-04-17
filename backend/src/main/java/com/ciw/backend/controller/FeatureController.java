3d8108a805cbb1b3f410b16d3b6538994fbb4dc4
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-04-17 15:14:44 +0700

    dev: add features handler

---
+++ backend/src/main/java/com/ciw/backend/controller/FeatureController.java
@@ -0,0 +1,43 @@
+package com.ciw.backend.controller;
+
+import com.ciw.backend.payload.SimpleListResponse;
+import com.ciw.backend.payload.feature.FeatureResponse;
+import com.ciw.backend.service.FeatureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/feature")
+@RequiredArgsConstructor
+@Tag(
+		name = "Feature"
+)
+@Validated
+public class FeatureController {
+	private final FeatureService featureService;
+
+	@GetMapping
+	@SecurityRequirement(
+			name = "Bearer Authentication"
+	)
+	@Operation(
+			summary = "Fetch all features",
+			description = "Fetch all features from database"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "Http Status is 200 OK"
+	)
+	public ResponseEntity<SimpleListResponse<FeatureResponse>> getAllFeatures() {
+		return new ResponseEntity<>(featureService.getAllFeatures(), HttpStatus.OK);
+	}
+}
---
+++ backend/src/main/java/com/ciw/backend/service/FeatureService.java
@@ -0,0 +1,31 @@
+package com.ciw.backend.service;
+
+import com.ciw.backend.entity.Feature;
+import com.ciw.backend.payload.SimpleListResponse;
+import com.ciw.backend.payload.feature.FeatureResponse;
+import com.ciw.backend.repository.FeatureRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FeatureService {
+	private final FeatureRepository featureRepository;
+	private final ObjectMapper objectMapper;
+
+	public SimpleListResponse<FeatureResponse> getAllFeatures() {
+		List<FeatureResponse> features = featureRepository.findAll().stream().map(this::mapToDTO).toList();
+		return new SimpleListResponse<>(features);
+	}
+
+	private Feature mapToEntity(FeatureResponse featureResponse) {
+		return objectMapper.convertValue(featureResponse, Feature.class);
+	}
+
+	private FeatureResponse mapToDTO(Feature feature) {
+		return objectMapper.convertValue(feature, FeatureResponse.class);
+	}
+}
