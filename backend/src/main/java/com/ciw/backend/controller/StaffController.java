3ac97ad64397c8632425d8405f3e1888fd9b8794
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-04-18 18:55:43 +0700

    update: add role for controller

--- backend/src/main/java/com/ciw/backend/controller/StaffController.java
+++ backend/src/main/java/com/ciw/backend/controller/StaffController.java
@@ -1,8 +1,10 @@
 package com.ciw.backend.controller;
 
 import com.ciw.backend.payload.ListResponse;
+import com.ciw.backend.payload.SimpleResponse;
 import com.ciw.backend.payload.page.AppPageRequest;
 import com.ciw.backend.payload.user.CreateUserRequest;
+import com.ciw.backend.payload.user.UpdateUserRequest;
 import com.ciw.backend.payload.user.UserFilter;
 import com.ciw.backend.payload.user.UserResponse;
 import com.ciw.backend.service.StaffService;
@@ -14,6 +16,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,6 +42,7 @@ public class StaffController {
 			responseCode = "200",
 			description = "Http Status is 200 OK"
 	)
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
 	public ResponseEntity<ListResponse<UserResponse, UserFilter>> getUsers(
 			@Valid AppPageRequest page,
 			@Valid UserFilter filter) {
@@ -57,10 +61,12 @@ public class StaffController {
 			responseCode = "200",
 			description = "Http Status is 200 OK"
 	)
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
 	public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
 		return new ResponseEntity<>(staffService.getUser(id), HttpStatus.OK);
 	}
 
+	@PostMapping
 	@SecurityRequirement(
 			name = "Bearer Authentication"
 	)
@@ -72,10 +78,49 @@ public class StaffController {
 			responseCode = "201",
 			description = "Http Status is 201 CREATED"
 	)
-	@PostMapping
-	public ResponseEntity<UserResponse> createUnit(
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
+	public ResponseEntity<UserResponse> createStaff(
 			@Valid @RequestBody CreateUserRequest request
 	) {
-		return new ResponseEntity<>(staffService.createUser(request), HttpStatus.CREATED);
+		return new ResponseEntity<>(staffService.createStaff(request), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/{id}")
+	@SecurityRequirement(
+			name = "Bearer Authentication"
+	)
+	@Operation(
+			summary = "Update staff",
+			description = "Update staff"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "Http Status is 200 OK"
+	)
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
+	public ResponseEntity<UserResponse> updateStaff(
+			@PathVariable Long id,
+			@Valid @RequestBody UpdateUserRequest request
+	) {
+		return new ResponseEntity<>(staffService.updateStaff(id, request), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	@SecurityRequirement(
+			name = "Bearer Authentication"
+	)
+	@Operation(
+			summary = "Delete staff",
+			description = "Delete staff by id"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "Http Status is 200 OK"
+	)
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
+	public ResponseEntity<SimpleResponse> deleteStaff(
+			@PathVariable Long id
+	) {
+		return new ResponseEntity<>(staffService.deleteStaff(id), HttpStatus.OK);
 	}
 }
