f3b0c04cbb4e2f4156e79e6335e3ad0d5a84fd9b
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-04-18 18:57:44 +0700

    fix: update tag

---
+++ backend/src/main/java/com/ciw/backend/payload/tag/UpdateTagRequest.java
@@ -0,0 +1,19 @@
+package com.ciw.backend.payload.tag;
+
+import com.ciw.backend.constants.Message;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateTagRequest {
+	@Schema(name = "name", example = "Sự kiện")
+	@Length(min = 1, max = 50, message = Message.Tag.TAG_NAME_INVALID)
+	private String name;
+}
