b2a87ff7642b9ce022bbde32e6f36ceb9c45007d
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-05-02 17:46:31 +0700

    add notePicker

---
+++ frontend/components/news/notePicker.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React, { useState } from 'react'
+import Tiptap from './tiptap'
+import { v4 as uuidv4 } from 'uuid'
+
+const Todo = () => {
+  const [content, setContent] = useState<string>('')
+  const handleContentChange = (reason: any) => {
+    setContent(reason)
+  }
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    const data = {
+      id: uuidv4(),
+      content: content,
+    }
+    console.log(data)
+    const existingDataString = localStorage.getItem('myData')
+    const existingData = existingDataString
+      ? JSON.parse(existingDataString)
+      : []
+    const updatedData = [...existingData, data]
+    localStorage.setItem('myData', JSON.stringify(updatedData))
+    setContent("")
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-3xl w-full grid place-items-center mx-auto p-8 mb-10 rounded-md bg-white"
+    >
+      <Tiptap
+        content={content}
+        onChange={(newContent: string) => handleContentChange(newContent)}
+      />
+    </form>
+  )
+}
+
+export default Todo
