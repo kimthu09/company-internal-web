c018982f490d9985dc3bee54640fcc7ec7ee20fa
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-05-03 22:04:29 +0700

    fix: post filter

--- backend/src/main/java/com/ciw/backend/payload/post/PostSpecs.java
+++ backend/src/main/java/com/ciw/backend/payload/post/PostSpecs.java
@@ -6,7 +6,11 @@ import com.ciw.backend.exception.AppException;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -20,25 +24,25 @@ public class PostSpecs {
 	}
 
 	public static Specification<Post> isUpdatedAfter(String updatedAtFromDate) {
-		LocalDateTime updatedAtFrom;
+		Date fromDate;
 		try {
-			updatedAtFrom = LocalDateTime.parse(updatedAtFromDate,
-												DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+			fromDate = Date.valueOf(LocalDate.parse(updatedAtFromDate,
+													DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 		} catch (Exception e) {
 			throw new AppException(HttpStatus.BAD_REQUEST, Message.TIME_INVALID_FORMAT_DD_MM_YYYY);
 		}
-		return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("updatedAt"), updatedAtFrom);
+		return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("updatedAt"), fromDate);
 	}
 
 	public static Specification<Post> isUpdatedBefore(String updatedAtToDate) {
-		LocalDateTime updatedAtTo;
+		Timestamp toDate;
 		try {
-			updatedAtTo = LocalDateTime.parse(updatedAtToDate,
-											  DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+			LocalDate date = LocalDate.parse(updatedAtToDate, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+			LocalDateTime endOfDay = date.atTime(LocalTime.MAX);
+			toDate = Timestamp.valueOf(endOfDay);
 		} catch (Exception e) {
 			throw new AppException(HttpStatus.BAD_REQUEST, Message.TIME_INVALID_FORMAT_DD_MM_YYYY);
 		}
-
-		return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("updatedAt"), updatedAtTo);
+		return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("updatedAt"), toDate);
 	}
 }
