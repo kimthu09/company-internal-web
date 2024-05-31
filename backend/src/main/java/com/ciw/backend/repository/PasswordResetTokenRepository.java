59fa324eb3af74bc634c07dc25bb91fbf313c09f
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-05-31 09:56:38 +0700

    dev: CI/CD workflow

--- backend/src/main/java/com/ciw/backend/repository/PasswordResetTokenRepository.java
+++ backend/src/main/java/com/ciw/backend/repository/PasswordResetTokenRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, String> {
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Integer> {
 	Optional<PasswordResetToken> findByToken(String token);
 }
